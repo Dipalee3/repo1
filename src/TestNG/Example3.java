b7136c930f9902fda2619633a5cad9a79074579d
Dipalee <dipaleepatil1@gmail.com>
2024-08-22 22:02:30 +0530

    Develop meassage done

--- src/TestNG/Example3.java
+++ src/TestNG/Example3.java
@@ -9,16 +9,4 @@ public class Example3 {
     	 System.out.println("Running test 7");
      }
      
-     @Test
-     public void test8()
-     {
-    	 System.out.println("Running test 8");
-    	 
-     }
-     
-     @Test
-     public void test9()
-     {
-    	 System.out.println("Running test 9");
-}
 }
